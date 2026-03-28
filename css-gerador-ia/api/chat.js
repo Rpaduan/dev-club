b825fdc07fd8c325cb7a59c28c41dc8998ba4244
Rpaduan <renato_paduan@hotmail.com>
2026-03-28 15:13:31 -0300

    link groq

--- css-gerador-ia/api/chat.js
+++ css-gerador-ia/api/chat.js
@@ -1,9 +1,10 @@
 // api/chat.js
+
 export default async function handler(req, res) {
   // Pega a API KEY das variáveis de ambiente da Vercel (protegido)
   const apiKey = process.env.GROQ_API_KEY;
 
-  const response = await fetch("https://api.groq.com", {
+  const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
     method: "POST",
     headers: {
       "Authorization": `Bearer ${apiKey}`,
